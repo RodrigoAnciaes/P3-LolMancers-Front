ce41902a31a0ecc7b810f2a4007bfba08d3b571b
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2022-11-29 09:06:09 -0300

    Arrumando como o campeão com a maior maestria é pego no servidor, nn estando mais armazenado no código.

--- src/Paginas/Maestria/maestria.js
+++ src/Paginas/Maestria/maestria.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as config from '../../config.js'
 import Navbar from '../../components/Navbar/navbar'; 
-import data from './champion.json';
 
 export default function Maestria(props){
     const [maestria,setMaestria]=useState({});
+    const [champion,setChampion]=useState({});
 
     function getSummonerId(name){
         const linkSummonerId = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+"?api_key="+config.API_KEY;
@@ -21,34 +21,25 @@ export default function Maestria(props){
         const summonerId = await getSummonerId(name); 
         const linkMastery = "https://br1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+summonerId+"?api_key="+config.API_KEY;
         // console.log(linkMastery);
-        const mastery = await axios.get(linkMastery)
-            .then(function(response){
-                // console.log(response.data)
-                return response.data})
-            .catch(function(erro){
-                console.log(erro)})
-        console.log(mastery[0]);
+        axios.get(linkMastery)
+            .then(function(mastery){
+        console.log(mastery.data);
+        const championId = mastery.data[0].championId;
+        const championPoints = mastery.data[0].championPoints;
+        console.log("championId: "+championId);
+        console.log("championPoints: "+championPoints);
 
-        const championId = mastery[0].championId;
-        const championPoints = mastery[0].championPoints;
-
-        // const championNameById = "https://ddragon.leagueoflegends.com/cdn/8.1.1/data/en_US/champion.json"   
-        // const championName = await axios.get(championNameById)
-        //     .then(function(response){  
-        //         console.log(response.data.data);
-        //         return response.data.data})
-        //     .catch(function(erro){
-        //         console.log(erro)})
-
-        for (const [champion, details] of Object.entries(data.data)){
-            if (details.key == championId){
-                console.log(details.name);
-                setMaestria({name: champion, points: championPoints});
-                console.log(maestria);
-            }
+        const linkChampion = "http://ddragon.leagueoflegends.com/cdn/11.6.1/data/pt_BR/champion.json";
+        axios.get(linkChampion)
+            .then(function(champion){
+                // the champion object has the champion name as its key and contains the champion data including the champion id named key
+            const championName = Object.keys(champion.data.data).find(key => champion.data.data[key].key == championId);
+            const championImage = champion.data.data[championName].image.full;
+            setChampion({name: championName, image: championImage});
+            setMaestria({championName: championName, championPoints: championPoints});
         }
-
-    }
+                ).catch(function(erro){console.log(erro)})
+}).catch(function(erro){console.log(erro)});}
 
 
     useEffect(()=>{
@@ -63,7 +54,9 @@ export default function Maestria(props){
                 <h2 className="maestria-title">Maestria de {props.name}</h2> 
                 <div className="maestria-content">
                     <div className="maestria-one">
-                        <span> <strong>Campeão:</strong> {maestria.name} <strong>Pontos:</strong> {maestria.points}</span>
+                        <img className="maestria-img" src={"http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/"+champion.image} alt="champion"/>
+                        <h3 className="maestria-champion">{champion.name}</h3>
+                        <h3 className="maestria-points">{maestria.championPoints}</h3>
                     </div>
                 </div>  
             </div>
